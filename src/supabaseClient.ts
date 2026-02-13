232b238ca7905b9a60a2a548ad594276617c53fc
Fabricio Fernando <fabriciofernt@gmail.com>
2026-02-13 14:06:46 -0300

    Replace supabaseKey with supabaseAnonKey

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Usa variáveis de ambiente para segurança
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Inicializa o cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
