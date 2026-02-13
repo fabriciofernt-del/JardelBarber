b7a65765f4673d079f125bfd2fbeaf06b86f900f
Fabricio Fernando <fabriciofernt@gmail.com>
2026-02-13 16:14:05 -0300

    Delete src/supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Usa variáveis de ambiente para segurança
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Inicializa o cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
